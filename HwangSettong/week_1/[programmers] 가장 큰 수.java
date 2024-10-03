17ddf98356b74cfc63f55af9f3ce26358700c53b
HwangSettong <94304576+HwangSettong@users.noreply.github.com>
2024-10-04 04:23:04 +0900

    Add: [programmers] 가장 큰 수.java
    Add: [programmers] 가장 큰 수.java

---
+++ HwangSettong/week_1/[programmers] 가장 큰 수.java
@@ -0,0 +1,17 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java#
+import java.util.*;
+import java.lang.*;
+class Solution {
+    public String solution(int[] numbers) {
+        ArrayList<String> a = new ArrayList<>();
+        int check = 0;
+        for(int n: numbers){
+            check+=n;
+            a.add(String.valueOf(n));
+        }
+        if(check == 0){ return "0";}
+        
+        a.sort( (o1, o2) -> Integer.parseInt(o2+o1) - Integer.parseInt(o1+o2));
+        return String.join("",a);
+    }
+}
