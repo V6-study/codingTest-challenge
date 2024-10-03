c692e5891ff7c683815f196d55225ca28f35cec8
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-10-03 10:24:14 +0900

    Style: 띄어쓰기 수정

--- kiv9908/week_1/116p.py
+++ kiv9908/week_1/116p.py
@@ -1,10 +1,10 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3
 def solution(answers):
     answer = []
-    arr1 = [1,2,3,4,5]
+    arr1 = [1, 2, 3, 4, 5]
     arr2 = [2, 1, 2, 3, 2, 4, 2, 5]
     arr3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    score = [0,0,0]
+    score = [0, 0, 0]
     for i in range(len(answers)):
         if answers[i] == arr1[i%len(arr1)]:
             score[0] += 1
