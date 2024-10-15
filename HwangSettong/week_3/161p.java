6fb334e9566a21deea044681e8ce7023d7ec771b
HwangSettong <94304576+HwangSettong@users.noreply.github.com>
2024-10-15 19:10:14 +0900

    Add: 161p.java

---
+++ HwangSettong/week_3/161p.java
@@ -0,0 +1,16 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=java
+import java.util.*;
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for(int i = 0 ; i < prices.length-1; i++){
+            for(int j = i+1; j< prices.length; j++){
+                answer[i] = j-i;
+                if(prices[i] > prices[j]){
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
