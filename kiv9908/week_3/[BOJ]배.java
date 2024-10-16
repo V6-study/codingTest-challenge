65b57aabb6569a39a1db2d8f0edcf5a99b2d6b63
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-10-16 15:27:24 +0900

    Add: [BOJ]배.java

---
+++ kiv9908/week_3/[BOJ]배.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer> weight = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            weight.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(weight, Collections.reverseOrder());
+
+        int m = Integer.parseInt(br.readLine());
+
+        List<Integer> boxes = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            boxes.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(boxes, Collections.reverseOrder());
+
+        if (boxes.get(0) > weight.get(0)) {
+            System.out.println(-1);
+            return;
+        }
+
+        int rounds = 0;
+        while (!boxes.isEmpty()) {
+            int boxIndex = 0;
+            for (int i = 0; i < n && boxIndex < boxes.size(); ) {
+                // 크레인이 들 수 있는 가장 무거운 박스를 찾아서 처리
+                if (boxes.get(boxIndex) <= weight.get(i)) {
+                    boxes.remove(boxIndex);
+                    i++;
+                } else {
+                    boxIndex++;
+                }
+            }
+            rounds++;
+        }
+
+        System.out.println(rounds);
+    }
+}
