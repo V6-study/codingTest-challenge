e1f0b232578773b9e4f8f8befe29fd851a0bc694
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-10-17 23:36:11 +0900

    Add: [BOJ] 꿀따기.java

---
+++ kiv9908/week_3/[BOJ] 꿀따기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long[] honey = new long[n];
+        long totalHoney = 0;
+        long answer = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            honey[i] = Long.parseLong(st.nextToken());
+            totalHoney += honey[i];
+        }
+
+        // 벌 왼쪽 고정, 꿀통 오른쪽 고정
+        long fixSum = totalHoney - honey[0];
+        long moveSum = fixSum;
+        for (int i = 1; i < n-1; i++) {
+            long sum = fixSum - honey[i];
+            moveSum -= honey[i];
+            sum += moveSum;
+            answer = Math.max(answer, sum);
+        }
+
+        // 벌 오른쪽 고정, 꿀통 왼쪽 고정
+        fixSum = totalHoney - honey[n-1];
+        moveSum = fixSum;
+        for (int i = n-2; i > 0; i--) {
+            long sum = fixSum - honey[i];
+            moveSum -= honey[i];
+            sum += moveSum;
+            answer = Math.max(answer, sum);
+        }
+
+        // 벌 양쪽, 꿀통 움직임
+        long bee1 = 0;
+        long bee2 = totalHoney - honey[n-1] ;
+        for (int i = 1; i < n-1; i++) {
+            bee1 += honey[i];
+            bee2 -= honey[i-1];
+            answer = Math.max(answer, bee1+bee2);
+        }
+        System.out.println(answer);
+    }
+}
