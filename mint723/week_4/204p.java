28c1614d7921cced44c584cdb5745a982900c2b3
mint723 <114153768+mint723@users.noreply.github.com>
2024-10-21 11:30:12 +0900

    Add: 204p.java

---
+++ mint723/week_4/204p.java
@@ -0,0 +1,29 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42586
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<progresses.length; i++){
+            int completedDate = (int)Math.ceil((100 - progresses[i]) / (speeds[i]*1.0));
+            q.offer(completedDate);
+        }
+        
+        while(!q.isEmpty()){
+            int cnt = 1;
+            int num = q.poll();
+            
+            while(!q.isEmpty() && q.peek() <= num){
+                q.poll();
+                cnt++;
+            }
+            list.add(cnt);
+        }
+        
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
