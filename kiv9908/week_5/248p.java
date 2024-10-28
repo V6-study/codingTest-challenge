2036909eb99ff543c51540e80d4e7d1573063971
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-10-28 15:32:18 +0900

    Add: 248p.java

---
+++ kiv9908/week_5/248p.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public List<String> solution(String[] record) {
+        
+        Map<String, String> user = new HashMap<>();
+        
+        for (int i = 0; i<record.length; i++){
+            String[] info = record[i].split(" ");
+            if(!info[0].equals("Leave")){
+                user.put(info[1],info[2]);
+            }
+            
+        }
+        List<String> answer = new ArrayList<>();
+        
+        for (int i = 0; i<record.length; i++){
+            String[] info = record[i].split(" ");
+            if(info[0].equals("Enter")){
+                answer.add(user.get(info[1])+"님이 들어왔습니다.");
+                }
+            else if(info[0].equals("Leave")){
+                answer.add(user.get(info[1])+"님이 나갔습니다.");
+            }
+        }
+        
+        
+        return answer;
+    }
+}
