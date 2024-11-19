5cdab141fa7d58b6affc72bab6de9441f46578e0
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-11-19 10:16:51 +0900

    Refactor: bfs로직

--- kiv9908/week_7/402p.java
+++ kiv9908/week_7/402p.java
@@ -1,20 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static ArrayList<Integer>[] A;
-    private static int[] visited;
-    private static int cnt;
-
-    private static ArrayList<Integer> answer;
+    // 인접리스트
+    static ArrayList<Integer>[] A;
+    static int[] visited;
+    static int cnt;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -22,8 +22,10 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
         int R = Integer.parseInt(st.nextToken());
 
+        A = new ArrayList[N+1];
+        visited = new int[N + 1];
+        cnt = 1;
 
-        A = new ArrayList[N + 1];
         for (int i = 1; i < N + 1; i++) {
             A[i] = new ArrayList<>();
         }
@@ -36,31 +38,33 @@ public class Main {
             A[e].add(s);
         }
 
-        for (int i = 1; i < N + 1; i++) {
+        for (int i = 1; i < N+1; i++) {
             Collections.sort(A[i]);
         }
-
-        visited = new int[N + 1];
-        answer = new ArrayList<>();
-
-        cnt = 1;
-        DFS(R);
-
-        for (int i = 1; i < N + 1; i++) {
+        
+        BFS(R);
+        for(int i = 1; i < N+1; i++){
             System.out.println(visited[i]);
         }
-
-
     }
 
-    private static void DFS(int now) {
-        visited[now] = cnt;
-        for (int next : A[now]) {
-            if (visited[next] == 0) {
-                cnt++;
-                DFS(next);
-            }
+    public static void BFS(int start) {
+        // 탐색시 맨 처음 방문할 노드를 add 하고 방문 처리
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        queue.add(start);
+        visited[start] = cnt;
 
+        // 큐가 비어 있지 않은 동안 반복
+        while(!queue.isEmpty()) {
+            // 큐에 있는 원소 중 가장 먼저 추가된 원소를 poll
+            int now = queue.poll();
+            for(int next : A[now]){
+                if(visited[next]==0){ // 방문하지 않은 인접한 노드인 경우
+                    queue.add(next); // 큐에 넣고 방문처리
+                    cnt++;
+                    visited[next] = cnt;
+                }
+            }
         }
 
 
