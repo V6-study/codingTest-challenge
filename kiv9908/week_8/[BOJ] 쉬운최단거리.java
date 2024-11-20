f835cab5201e8113890be14e8dba49fcc30bf154
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-11-20 18:57:57 +0900

    Refactor: bfs 로직

--- kiv9908/week_8/[BOJ] 쉬운최단거리.java
+++ kiv9908/week_8/[BOJ] 쉬운최단거리.java
@@ -1,66 +1,68 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] graph;
+    static int n;
+    static int m;
+    static int[][] map;
+    static int targetX;
+    static int targetY;
     static boolean[][] visited;
-    static int[][] result;
-    static int n, m;
-    static int[] dx = {-1, 1, 0, 0}; // 상,하,좌,우
-    static int[] dy = {0, 0, -1, 1};
-    public static void main(String[] args) throws Exception{
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
-        graph = new int[n][m];
+        map = new int[n][m];
         visited = new boolean[n][m];
-        result = new int[n][m];
-        int x=0, y=0;
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < m; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
-                if (graph[i][j] == 2) {
-                    x = i;
-                    y = j;
-                } else if (graph[i][j] == 0) {
-                    visited[i][j] = true;
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 2){
+                    targetX = i;
+                    targetY = j;
                 }
             }
         }
-        bfs(x, y);
+        bfs(targetX,targetY);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!visited[i][j]) {
-                    result[i][j] = -1;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(!visited[i][j] && map[i][j]==1){
+                    sb.append("-1").append(" ");
+                }else{
+                    sb.append(map[i][j]).append(" ");
                 }
-                System.out.print(result[i][j] + " ");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb);
+
     }
-
-    public static void bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x, y});
-        visited[x][y] = true;
-
-        while (!queue.isEmpty()) {
-            int tmp[] = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int nx = tmp[0] + dx[i];
-                int ny = tmp[1] + dy[i];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                    if (!visited[nx][ny] && graph[nx][ny] == 1) {
+    private static void bfs(int targetX, int targetY) {
+        Deque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{targetX, targetY});
+        visited[targetX][targetY] = true;
+        map[targetX][targetY] = 0;
+        while(!q.isEmpty()) {
+            int[] now = q.poll();
+            for(int i = 0; i < 4; i++) {
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+                if(nx<n && ny<m && nx>=0 && ny>=0){
+                    if(map[nx][ny] != 0 && !visited[nx][ny]){
                         visited[nx][ny] = true;
-                        result[nx][ny] = result[tmp[0]][tmp[1]] + 1;
-                        queue.add(new int[]{nx, ny});
+                        q.offer(new int[]{nx, ny});
+                        map[nx][ny] = map[now[0]][now[1]] + 1;
                     }
                 }
             }
