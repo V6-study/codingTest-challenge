ba8e718a836c51c8bef16a9e5b781001008a8515
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-11-23 19:20:06 +0900

    Refactor: 소스 코드 수정

--- kiv9908/week_7/405p.java
+++ kiv9908/week_7/405p.java
@@ -1,67 +1,72 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
+// 노드 class
+class Node {
+    int end;
+    int weight;
+
+    public Node(int end, int weight) {
+        this.end = end;
+        this.weight = weight;
+    }
+}
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
- 
-        int N = 0, M = 0, R = 0, start = 0, end = 0, order = 1, front = 0, num = 0;
-        int[] res;
-        boolean visited[];
-        List<List<Integer>> list = new ArrayList<>();
-        Queue<Integer> que = new LinkedList<Integer>();
- 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        R = Integer.parseInt(st.nextToken());
- 
-        for (int i = 0; i <= N; i++)
-            list.add(new ArrayList<>());
-        visited = new boolean[N + 1];
-        res = new int[N + 1];
- 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            start = Integer.parseInt(st.nextToken());
-            end = Integer.parseInt(st.nextToken());
- 
-            list.get(start).add(end);
-            list.get(end).add(start);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(br.readLine());
+
+        boolean[] visit = new boolean[V+1];     // 방문 처리 배열
+        int[] result = new int[V+1];            // 최단 경로 값 저장 배열
+        List<Node>[] list = new List[V+1];      // 연결 정보 저장 배열
+		
+        // 연결 정보 저장할 배열, 최단 경로 값 저장 배열 초기화
+        for(int i = 1; i<=V; i++) {
+            list[i] = new ArrayList<>();
+            result[i] = Integer.MAX_VALUE;
         }
- 
-        for (int i = 0; i < list.size(); i++)
-            Collections.sort(list.get(i));
- 
-        que.add(R);
-        visited[R] = true;
-        res[R] = order++;
- 
-        while (!que.isEmpty()) {
-            front = que.poll();
- 
-            for (int i = 0; i < list.get(front).size(); i++) {
-                num = list.get(front).get(i);
-                if (visited[num] == false) {
-                    visited[num] = true;
-                    que.add(num);
-                    res[num] = order++;
+
+        for(int i = 0; i<E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());       // 출발
+            int v = Integer.parseInt(st.nextToken());       // 도착
+            int w = Integer.parseInt(st.nextToken());       // 가중치
+
+            list[u].add(new Node(v, w));
+        }
+
+        // 다익스트라
+        PriorityQueue<Node> queue = new PriorityQueue<>((o1, o2) -> o1.weight - o2.weight);
+        result[K] = 0;
+        queue.add(new Node(K, 0));
+
+        while(!queue.isEmpty()) {
+            Node now = queue.poll();		// 현재 방문 정점
+            if(!visit[now.end]) visit[now.end] = true;		// 방문처리
+            // 현재 정정과 연결된 간선들에 대해 판단
+            for(int i = 0; i<list[now.end].size(); i++) {
+            	// 현재 정점에서 이어질 다음 정점
+                Node next = list[now.end].get(i);
+                // 다음 정점이 방문하지 않았고, 
+                // 현재 가중치 + 해당 정점으로 향하는 가중치 값 < 해당 정점으로의 최단 경로 값이라면
+                if(!visit[next.end] && now.weight + next.weight < result[next.end]) {
+                    // 해당 정점으로의 최단 경로 값 Update
+                    result[next.end] = now.weight + next.weight;
+                    // 다음 방문할 예정이므로 queue에 넣어주기
+                    queue.add(new Node(next.end, result[next.end]));
                 }
             }
         }
- 
-        for (int i = 1; i < res.length; i++)
-            sb.append(res[i]).append("\n");
- 
-        System.out.println(sb);
+		
+        // 출력
+        for(int i = 1; i<=V; i++) {
+            if(result[i] == Integer.MAX_VALUE) System.out.println("INF");
+            else System.out.println(result[i]);
+        }
     }
 }
