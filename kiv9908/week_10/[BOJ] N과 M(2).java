dfc997cef8551b58b9f4ef8a2c52c4d6a0919e15
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-12-06 10:46:28 +0900

    Refactor: vistied 배열 추가

--- kiv9908/week_10/[BOJ] N과 M(2).java
+++ kiv9908/week_10/[BOJ] N과 M(2).java
@@ -1,44 +1,44 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.io.IOException;
- 
+
 public class Main {
- 
-	public static int[] arr;
-	public static int N, M;
-	public static StringBuilder sb = new StringBuilder();
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
- 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
- 
-		arr = new int[M];
-        
-		dfs(1, 0);
-		System.out.println(sb);
- 
-	}
- 
-	public static void dfs(int at, int depth) {
- 
-		if (depth == M) {
-			for (int val : arr) {
-				sb.append(val).append(' ');
-			}
-			sb.append('\n');
-			return;
-		}
-        
-		for (int i = at; i <= N; i++) {
- 
-			arr[depth] = i;
-			dfs(i + 1, depth + 1);
- 
-		}
-	}
+    static int n;
+    static int m;
+    static boolean[] visited;
+    static int[] answer;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        visited = new boolean[n+1];
+        answer = new int[m];
+        int depth = 0;
+        int start = 1;
+        dfs(start, depth);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int start, int depth) {
+        if (m == depth) {
+            for (int i : answer) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < n + 1; i++) {
+            if(!visited[i]){
+                visited[i] = true;
+                answer[depth] = i;
+                dfs(i+1,depth + 1);
+                visited[i] = false;
+            }
+
+        }
+    }
 }
