f952c386594226b6cc7c86ab7039287f5b9feb26
mint723 <114153768+mint723@users.noreply.github.com>
2024-12-12 17:00:52 +0900

    Add: 547p.java

---
+++ mint723/week_11/547p.java
@@ -0,0 +1,19 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42748
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i=0; i<commands.length; i++){
+            List<Integer> list = Arrays.stream(array).boxed().collect(Collectors.toList());
+            List<Integer> subList = list.subList(commands[i][0]-1, commands[i][1]);
+            Collections.sort(subList);
+            
+            answer[i] = subList.get(commands[i][2]-1);
+        }
+        
+        return answer;
+    }
+}
