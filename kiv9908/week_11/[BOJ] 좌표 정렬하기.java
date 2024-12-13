b150a74cba19aca8a768908b12f014e6e7661a1b
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-12-13 23:01:29 +0900

    Add: [BOJ] 좌표 정렬하기.java

---
+++ kiv9908/week_11/[BOJ] 좌표 정렬하기.java
@@ -0,0 +1,38 @@
+package week_11.좌표정렬하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        List<int[]> list = new ArrayList<>();
+        for(int i = 0;i < n;i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            list.add(new int[]{x,y});
+        }
+        list.sort((o1,o2) -> {
+            int x1 = o1[0];
+            int x2 = o2[0];
+            if(x1 == x2) {
+                int y1 = o1[1];
+                int y2 = o2[1];
+                return Integer.compare(y1,y2);
+            }
+            return Integer.compare(x1,x2);
+        });
+        StringBuilder sb = new StringBuilder();
+        for (int[] xy : list) {
+            sb.append(xy[0]).append(" ").append(xy[1]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
