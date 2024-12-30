5fd97aeaf292a50119f7cd66d64898636b349976
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-12-30 21:59:55 +0900

    Add: 598p.java

---
+++ kiv9908/week_13/598p.java
@@ -0,0 +1,15 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120861
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] now = {0, 0};
+        
+        for (int i = 0; i < keyinput.length; i++){
+            if(keyinput[i].equals("left")) now[0] -= now[0]>-(board[0]/2)?1:0;
+            else if(keyinput[i].equals("right")) now[0] += now[0]<(board[0]/2)?1:0;
+            else if(keyinput[i].equals("down")) now[1] -= now[1]>-(board[1]/2)?1:0;
+            else if(keyinput[i].equals("up")) now[1] += now[1]<(board[1]/2)?1:0;
+        
+        }
+        return now;
+    }
+}
