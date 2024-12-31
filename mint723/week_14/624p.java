b8a36184c308262903bbaaaf1159816aa1a6537d
mint723 <114153768+mint723@users.noreply.github.com>
2024-12-31 23:57:59 +0900

    Add: 624p.java

---
+++ mint723/week_14/624p.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution("ABCBDAB", "BDCAB"));
+        System.out.println(solution.solution("AGGTAB", "GXTXAYB"));
+    }
+
+    public int solution(String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
+
+        int[][] dp = new int[m+1][n+1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(str1.charAt(i - 1) == str2.charAt(j - 1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
