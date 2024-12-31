89fc6f862532b09df1990c15539bc27c8a3559ff
Jane Kim <90606616+jane-kim-279@users.noreply.github.com>
2025-01-01 02:41:21 +0900

    Add: 624p.java

---
+++ jane-kim-279/week_14/624p.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1.charAt(i -1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
