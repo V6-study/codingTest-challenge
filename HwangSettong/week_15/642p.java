abf2caf06b545a728d5bad43d6fd384dba5f976a
HwangSettong <94304576+HwangSettong@users.noreply.github.com>
2025-01-09 18:32:27 +0900

    Add: 642p.java

---
+++ HwangSettong/week_15/642p.java
@@ -0,0 +1,17 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12913
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        int[][] dp = new int[land.length][4];
+        dp[0] = land[0];
+        for(int i = 1; i<land.length; i++){
+            for(int j = 0; j<4; j++){
+                for(int k = 0; k<4; k++){
+                    if(k != j) dp[i][j] = Math.max(dp[i][j], land[i][j] + dp[i-1][k]);
+                }
+                answer = Math.max(answer, dp[i][j]);
+            }
+        }
+        return answer;
+    }
+}
