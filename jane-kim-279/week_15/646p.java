5a89df932a9db71d06e44b6d72fd67e224dfd5a4
Jane Kim <90606616+jane-kim-279@users.noreply.github.com>
2025-01-11 05:11:45 +0900

    Update 646p.java

--- jane-kim-279/week_15/646p.java
+++ jane-kim-279/week_15/646p.java
@@ -9,7 +9,7 @@ class Solution {
         dp1[0] = money[0];
         dp1[1] = money[0];
         for (int i = 2; i < n - 1; i++) {
-            dp1[i] = Math.max(dp[i - 1], dp[i - 2] + money[i]);
+            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + money[i]);
         }
 
         dp2[1] = money[1];
