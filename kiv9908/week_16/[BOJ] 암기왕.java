7007669e4175149af8812075208e17faa9ca9757
kevinlee <105219462+kiv9908@users.noreply.github.com>
2025-01-13 18:25:22 +0900

    Add: [BOJ] 암기왕.java

---
+++ kiv9908/week_16/[BOJ] 암기왕.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb;
+        int t = Integer.parseInt(br.readLine());
+        for(int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            sb = new StringBuilder();
+            Set<Integer> yeonjong = new HashSet<>();
+            for(int j = 0; j < n; j++) {
+                yeonjong.add(Integer.parseInt(st.nextToken()));
+            }
+            int m = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            int[] donggyu = new int[m];
+            for(int j = 0; j < m; j++) {
+                donggyu[j] = Integer.parseInt(st.nextToken());
+            }
+            for(int j = 0; j < m; j++) {
+                if (yeonjong.contains(donggyu[j])) {
+                    sb.append("1").append("\n");
+                } else {
+                    sb.append("0").append("\n");
+                }
+            }
+            System.out.print(sb);
+
+        }
+
+    }
+}
