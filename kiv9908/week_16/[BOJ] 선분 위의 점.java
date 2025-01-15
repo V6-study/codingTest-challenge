a02dfa2ad768229004ceb7e3ae0485768f1cdc9c
kevinlee <105219462+kiv9908@users.noreply.github.com>
2025-01-15 12:10:00 +0900

    Add: [BOJ] 선분 위의 점.java

---
+++ kiv9908/week_16/[BOJ] 선분 위의 점.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] points = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            points[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(points);
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            int lower = lowerBound(points, start);
+            int upper = upperBound(points, end);
+            sb.append(upper - lower).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static int lowerBound(int[] points, int target) {
+        int low = 0;
+        int high = points.length;
+        while(low < high) {
+            int mid = (low + high) / 2;
+            if(points[mid] < target) {
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    private static int upperBound(int[] points, int target) {
+        int low = 0;
+        int high = points.length;
+        while(low < high) {
+            int mid = (low + high) / 2;
+            if(points[mid] <= target) {
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+        }
+        return low;
+    }
