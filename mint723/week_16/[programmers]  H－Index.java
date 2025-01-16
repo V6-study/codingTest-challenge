063f24bf215ddb3cdf12a05acc285cbc5e859a73
mint723 <114153768+mint723@users.noreply.github.com>
2025-01-17 02:14:53 +0900

    Add: [programmers]  H－Index.java

---
+++ mint723/week_16/[programmers]  H－Index.java
@@ -0,0 +1,20 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42747
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 1;
+        int h=0;
+        for (int i = 0; i < citations.length; i++) {
+			answer=0;
+			for (int j = 0; j < citations.length; j++) {
+				if (citations[j]>=h) {
+					answer++;
+				}
+			}
+			if(h<answer) {
+				h++;
+			}
+		}
+        return answer;
+    }
+}
