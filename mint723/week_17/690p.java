9abffcd982a53a7f4d8c8496b9babf1bada86bc4
mint723 <114153768+mint723@users.noreply.github.com>
2025-01-19 16:14:16 +0900

    Add: 690p.java

---
+++ mint723/week_17/690p.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int n : tangerine){
+            map.put(n, map.getOrDefault(n, 0) +1);
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int n : map.values()){
+            list.add(n);
+        }
+        
+        list.sort((o1, o2) -> o2-o1);
+        
+        for(int n : list){
+            if(k > 0){
+                answer++;
+                k-=n;
+            }else
+                return answer;
+        }
+        
+        return answer;
+    }
+}
