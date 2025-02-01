d2610eb6f489d065262c3362f9446abf6047d953
kevinlee <105219462+kiv9908@users.noreply.github.com>
2025-02-01 15:29:02 +0900

    Add: 717p.java

---
+++ kiv9908/week_18/717p.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int length = numbers.length;
+        boolean[] memo = new boolean[10];
+        
+        
+        for(int i = 0;i<length;i++){
+            memo[numbers[i]] = true;
+        }
+        
+        for(int i = 1;i<10;i++){
+            if(!memo[i]){
+                answer += i;
+            }
+        }
+        
+        return answer;
+    }
+}
