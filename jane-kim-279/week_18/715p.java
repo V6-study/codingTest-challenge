06324a78dba52e5d27ff328a8be781361446ec7e
Jane Kim <90606616+jane-kim-279@users.noreply.github.com>
2025-02-04 05:39:43 +0900

    Add: 715p.java

---
+++ jane-kim-279/week_18/715p.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+class Solution {
+    static int[] answer = new int[2];
+
+    public int[] solution(int[][] arr) {
+        int totalSize = arr.length;
+        dq(0, 0, totalSize, arr);
+
+        return answer;
+    }
+
+    public static void dq(int startX, int startY, int size, int[][] arr) {
+        if (check(startX, startY, size, arr)) {
+            answer[arr[startX][startY]]++;
+            return;
+        }
+        dq(startX, startY, size / 2, arr);
+        dq(startX + size / 2, startY, size / 2, arr);
+        dq(startX, startY + size / 2, size / 2, arr);
+        dq(startX + size / 2, startY + size / 2, size / 2, arr);
+    }
+
+    public static boolean check(int x, int y, int size, int[][] arr) {
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (arr[x][y] != arr[i][j]) return false;
+            }
+        }
+        return true;
+    }
+}
